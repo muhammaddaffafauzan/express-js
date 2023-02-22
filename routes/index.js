2a1bd0b14ac14bb56797a37210fa07047face88e
DaffaFauzan <99790338+muhammaddaffafauzan@users.noreply.github.com>
2023-02-22 16:36:13 +0700

    Delete index.js

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const routerProduk = require('./produk');
-
-// GET localhost:8000/produk => Ambil data semua produk
-router.use('/produk', routerProduk);
-
-module.exports = router; 
